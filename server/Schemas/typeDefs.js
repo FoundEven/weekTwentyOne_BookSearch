ff1db01b51484688175ab86ecf5a95d21f9cea28
Evan Rice <evancr2017@gmail.com>
2024-01-29 22:15:28 -0600

    added installs and typedefs and resolvers

---
+++ server/Schemas/typeDefs.js
@@ -0,0 +1,37 @@
+const typeDefs = `
+  type User {
+    _id: ID
+    name: String
+    email: String
+    bookCount: Integer
+    savedBooks: [String]!
+  }
+
+  type Book {
+    bookId: ID
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    # Because we have the context functionality in place to check a JWT and decode its data, we can use a query that will always find and return the logged in user's data
+    me: User
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    saveBook( bookId: ID!, authors: [String], description: String, title: String, image: String, link: String): User
+    removeBook(bookId: ID!): User
+  }
+`;
+
+module.exports = typeDefs;
